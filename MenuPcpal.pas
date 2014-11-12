cd1ef757b7451a2f04eb0c04d9c59172761451b8
Cesar Herrera <keze87@gmail.com>
2014-11-12 00:12:06 -0300

    Menu Parte 2

---
+++ MenuPcpal.pas
@@ -0,0 +1,68 @@
+unit MenuPcpal; 
+
+interface
+
+	Procedure Menu (var SalidaMenu : integer);
+
+implementation
+
+	Uses
+	
+		crt;
+		
+	Procedure Menu (var SalidaMenu : integer);
+		begin
+		
+			ClrScr;
+		
+			writeln;
+			writeln('**** Menu Principal ****');
+			writeln;
+			writeln('1) Visualizar el terreno.' {(Con su debida descripcion)});
+			writeln;
+			writeln('2) Visualizar las poblaciones.' {(Con su debida descripcion)});
+			writeln;
+			writeln('3) Visualizar los factores constantes.');
+			writeln;
+			writeln('4) Visualizar el mundo.');
+			writeln;
+			writeln('5) Simulacion');
+			writeln;
+			writeln('Ingrese la opcion deseada');
+			writeln;
+			
+			repeat
+			
+				readln(SalidaMenu);
+				
+			until ((SalidaMenu > 0) and (SalidaMenu < 6));
+			
+			if SalidaMenu = 5 then
+				begin
+				
+					ClrScr;
+					writeln;
+					writeln('**** Menu Simulacion ****');
+					writeln;
+					writeln('1) Retomar Simulacion.');
+					writeln;
+					writeln('2) Empezar Simulacion.');
+					writeln;
+					writeln('Ingrese la opcion deseada');
+					writeln;
+					
+					repeat
+			
+						readln(SalidaMenu);
+				
+					until ((SalidaMenu > 0) and (SalidaMenu < 3));
+					
+					case SalidaMenu of
+						1 : SalidaMenu := 5;
+						2 : SalidaMenu := 6;
+					end;
+				
+				end;
+			
+		end;
+end.			 
