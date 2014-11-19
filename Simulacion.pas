629d3ca370a4a4d1eb23a84f40872f8fe4788643
jalbarrac <jalbarracn@gmail.com>
2014-11-18 22:14:54 -0300

    Simulacion.pas
    Se agrega un procedimiento pausa, detiene al ciclo o procedimiento en el cual se lo llama hasta que se presiona una tecla (p en este caso);
    Se agrega un placeholder para las cosas que ocurren dia a dia en la simulacion, para que se vea como funcionan la pausa y la salida al menu.

--- Simulacion.pas
+++ Simulacion.pas
@@ -6,6 +6,10 @@ interface
 
 	Procedure ImprLineasMapa(var lineatexto:string);
 
+        Procedure ModifSimulacion(var SalidaMod:integer; var dias:integer);
+
+        Procedure Pausa(var caracter:char);
+
 
 
 
@@ -15,6 +19,49 @@ implementation
 	Uses
 
 		crt,ProceduresVarios,MenuPcpal;
+
+        Procedure Pausa(var caracter:char);
+
+        begin
+                repeat
+                        delay(100);
+                        until readkey='p';
+                        end;
+
+        Procedure ModifSimulacion(var SalidaMod:integer; var dias:integer);
+
+        var I:integer;
+                clchar:char;
+                Salida:integer;
+                begin
+
+                clrscr;
+                Salida:=1;
+                writeln('Esto simboliza las cosas que ocurren dia a dia');
+                writeln('Es el dia :');
+                writeln;
+                writeln;
+                writeln('Presionar P para pausar');
+                writeln('Presionar S para salir al menu');
+                for I:=1 to dias do
+                begin
+                        gotoxy(12,2);
+                        write(I);
+                        delay(2000);
+                        if keypressed then
+                        begin
+                                clchar:=readkey;
+                                        if clchar='p' then
+                                        Pausa(clchar);
+                                        if clchar='s' then
+                                        Menu(Salida);
+                                        end;
+                                        end;
+                                        Menu(Salida);
+                                        end;
+
+
+
 
 	Procedure ImprLineasMapa(var lineatexto:string);
 
@@ -50,7 +97,7 @@ implementation
 
 	Procedure MostrarMapa(var SalidaMapa:integer; var ruta:string);
 
-	var 
+	var
 
 		texto:string;
 		mundi:text;
@@ -70,13 +117,12 @@ implementation
 
 
 
-//		repeat
-//			LeerINT(SalidaMapa);
-//		until ((SalidaMapa > 0) and (SalidaMapa < 2));
-		readkey;
-//		case SalidaMapa of
-{		1 : }Menu(SalidaMapa);
-//		end;
+     //repeat
+       //	LeerINT(SalidaMapa);
+   //until ((SalidaMapa > 0) and (SalidaMapa < 3));
+ //             case SalidaMapa of
+ //        1 : Menu(SalidaMapa);   {guardar y volver al menu}
+              //  end;
 
 	end;
 
